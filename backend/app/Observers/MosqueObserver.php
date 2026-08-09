329ee736b522468b8a0518ba2a25c2b638d1bf61
gnggroup-tech <supportgnggroup@gmail.com>
2026-08-09 13:33:54 +0800

    feat: add mosque management foundation

---
+++ backend/app/Observers/MosqueObserver.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Mosque;
+use App\Services\AuditLogger;
+
+class MosqueObserver
+{
+    public function __construct(private readonly AuditLogger $auditLogger)
+    {
+    }
+
+    public function created(Mosque $mosque): void
+    {
+        $this->auditLogger->log('mosque.created', $mosque, ['code' => $mosque->code, 'name' => $mosque->name]);
+    }
+
+    public function updated(Mosque $mosque): void
+    {
+        $changes = collect($mosque->getChanges())->except(['updated_at'])->all();
+
+        if ($changes !== []) {
+            $this->auditLogger->log('mosque.updated', $mosque, ['changes' => $changes]);
+        }
+    }
+
+    public function deleted(Mosque $mosque): void
+    {
+        $this->auditLogger->log('mosque.deleted', $mosque, ['code' => $mosque->code, 'name' => $mosque->name]);
+    }
+}
